f9e0f425c86a6bea052216839a92c4fecce0a5d4
dave <dave@f95002ce-95f7-4f7f-9389-3f9229bab526>
2011-03-11 13:47:30 +0000

    compile now preprocessing the ucf based on the parameters.v
    git-svn-id: https://katfs.kat.ac.za/svnROACH2/gw/roach2_bsp@184 f95002ce-95f7-4f7f-9389-3f9229bab526

--- include/parameters.v
+++ include/parameters.v
@@ -1,9 +1,9 @@
 `ifndef PARAMETERS_VH
 `define PARAMETERS_VH
 
-`undef ENABLE_DDR3
-`undef ENABLE_QDR
-`undef ENABLE_GBE
-`undef ENABLE_TGE
+`define ENABLE_DDR3
+`define ENABLE_QDR
+`define ENABLE_GBE
+`define ENABLE_TGE
 
 `endif
