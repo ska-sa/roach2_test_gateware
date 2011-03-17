0c4c90c081b04bd9bfc3a79eca219e58d98e97eb
dave <dave@f95002ce-95f7-4f7f-9389-3f9229bab526>
2011-03-17 13:42:23 +0000

    Start to a real testbench
    git-svn-id: https://katfs.kat.ac.za/svnROACH2/gw/roach2_bsp@187 f95002ce-95f7-4f7f-9389-3f9229bab526

--- modules/gbe_udp/tb/hdl/TB_gbe_udp.v
+++ modules/gbe_udp/tb/hdl/TB_gbe_udp.v
@@ -7,6 +7,21 @@
 `define CPUCLK_PERIOD 10
 module TB_gbe_dup();
 
+
+  localparam LOCAL_MAC        = 48'h02_03_04_05_06_07;
+  localparam LOCAL_IP         = {8'd192, 8'd168, 8'd69, 8'd5};
+  localparam LOCAL_PORT       = 16'hdead;
+  localparam LOCAL_GATEWAY    = 8'd1;
+
+  /* Loopback test */
+  localparam APP_DEST_IP      = LOCAL_IP;
+  localparam APP_DEST_PORT    = LOCAL_PORT;
+
+  localparam APP_TX_FRAMESIZE = 4096;
+
+  localparam CPU_TX_FRAMESIZE = 512;
+  localparam CPU_FRAME        = 10;
+
 /**** Application Interface ****/
   wire        app_clk_i;
 
@@ -15,7 +30,6 @@ module TB_gbe_dup();
   wire        app_tx_eof;
   wire [31:0] app_tx_destip;
   wire [15:0] app_tx_destport;
-
   wire        app_tx_afull;
   wire        app_tx_overflow;
   wire        app_tx_rst;
@@ -26,7 +40,6 @@ module TB_gbe_dup();
   wire [31:0] app_rx_srcip;
   wire [15:0] app_rx_srcport;
   wire        app_rx_badframe;
-
   wire        app_rx_overrun;
   wire        app_rx_ack;
   wire        app_rx_rst;
@@ -64,17 +77,18 @@ module TB_gbe_dup();
   wire        wb_ack_o;
 
  gbe_udp #(
-   .LOCAL_ENABLE     (1),
-   .LOCAL_MAC        (48'h123456789abc),
-   .LOCAL_IP         ({8'd100, 8'd101, 8'd102, 8'd103}),
-   .LOCAL_PORT       (16'hdead),
-   .LOCAL_GATEWAY    (8'h12),
-   .PHY_CONFIG       (69),
-   .DIS_CPU_TX       (0),
-   .DIS_CPU_RX       (0),
-   .TX_LARGE_PACKETS (0),
-   .RX_DIST_RAM      (0),
-   .ARP_CACHE_INIT   (0)
+   .LOCAL_ENABLE      (1),
+   .LOCAL_MAC         (LOCAL_MAC),
+   .LOCAL_IP          (LOCAL_IP),
+   .LOCAL_PORT        (LOCAL_PORT),
+   .LOCAL_GATEWAY     (LOCAL_GATEWAY),
+   .CPU_PROMISCUOUS   (1),
+   .PHY_CONFIG        (0),
+   .DIS_CPU_TX        (0),
+   .DIS_CPU_RX        (0),
+   .TX_LARGE_PACKETS  (0),
+   .RX_DIST_RAM       (0),
+   .ARP_CACHE_INIT    (0)
   ) gbe_udp_inst (
     .app_clk(app_clk_i),
 
@@ -150,7 +164,19 @@ module TB_gbe_dup();
   wire mac_clk = (clk_counter % (`MACCLK_PERIOD)) < ((`MACCLK_PERIOD)/2);
   wire cpu_clk = (clk_counter % (`CPUCLK_PERIOD)) < ((`CPUCLK_PERIOD)/2);
 
-  /**** APP TX ****/
+  /************************* MODE *****************************/
+
+  reg  [4:0] mode;
+  reg [10:0] mode_done;
+  localparam MODE_TX_OVERFLOW = 0;
+  localparam MODE_TX_OVERWAIT = 1;
+  localparam MODE_RX_OVERRUN  = 2;
+  localparam MODE_RX_OVERWAIT = 3;
+  localparam MODE_CPU         = 4;
+
+
+  /************************* APP TX *****************************/
+
   assign app_clk_i = app_clk;
 
   reg app_rst;
@@ -158,43 +184,150 @@ module TB_gbe_dup();
     app_rst <= rst;
   end
 
-  reg [16:0] app_tx_counter;
+  reg [33:0] app_tx_counter;
   reg        app_tx_en;
 
+  /* we send a 32-bit counter */
+
+  reg wait_overflow_clear;
+
+  reg tx_overflow_rst;
+
   always @(posedge app_clk) begin
+    tx_overflow_rst <= 1'b0;
+
     if (app_rst) begin
-      app_tx_counter <= 16'h0;
+      mode_done[MODE_TX_OVERFLOW] <= 1'b0;
+      app_tx_counter <= 34'h0;
       app_tx_en <= 1'b0;
+      wait_overflow_clear <= 1'b0;
     end else begin
-      if (app_tx_afull)
-        app_tx_en <= 1'b0;
-      if (!app_tx_afull)
-        app_tx_en <= 1'b1;
+      case (mode)
+        MODE_TX_OVERFLOW: begin
+          if (wait_overflow_clear) begin
+            if (!app_tx_overflow)  begin
+              mode_done[MODE_TX_OVERFLOW] <= 1'b1;
+            end
+          end else begin
+            app_tx_en <= 1'b1;
+            if (app_tx_overflow) begin
+              tx_overflow_rst     <= 1'b1;
+              wait_overflow_clear <= 1'b1;
+              app_tx_en <= 1'b0;
+            end
+          end
+        end
+        default : begin
+          if (app_tx_afull)
+            app_tx_en <= 1'b0;
+          if (!app_tx_afull)
+            app_tx_en <= 1'b1;
 
-      if (app_tx_en)
-        app_tx_counter <= app_tx_counter + 1;
+          if (app_tx_en)
+            app_tx_counter <= app_tx_counter + 1;
+
+          if (app_tx_overflow) begin
+            $display("FAILED: unexpected tx buffer overflow");
+            $finish;
+          end
+        end
+      endcase
     end
   end
 
-  assign app_tx_data     = !app_tx_counter[0] ? app_tx_counter[16:9] : app_tx_counter[8:1];
+  assign app_tx_data     = app_tx_counter[1:0] == 0 : (app_tx_counter >>  2) & 8'hff:
+                           app_tx_counter[1:0] == 1 : (app_tx_counter >> 10) & 8'hff:
+                           app_tx_counter[1:0] == 2 : (app_tx_counter >> 18) & 8'hff:
+                           app_tx_counter[1:0] == 3 : (app_tx_counter >> 26) & 8'hff:
+
   assign app_tx_dvld     = app_tx_en && !app_rst;
-  assign app_tx_eof      = app_tx_counter[7:0] == {8{1'b1}};
-  assign app_tx_destip   = {8'd192, 8'd168, 8'd64, 8'd1};
-  assign app_tx_destport = 16'hbeef;
+  assign app_tx_eof      = mode == MODE_TX_OVERFLOW ? 1'b0:
+                           (app_tx_counter % APP_TX_FRAME_SIZE) == APP_TX_FRAME_SIZE - 1;
+  assign app_tx_destip   = APP_DEST_IP;
+  assign app_tx_destport = APP_DEST_PORT;
+  assign app_tx_rst      = app_rst || app_tx_overflow &&app_tx_en;
+
+  /************************* APP RX **************************/
+
+  reg app_rx_ack_reg;
+
+  reg        rx_counter_valid;
+  reg [31:0] rx_counter;
+
+  reg [31:0] rx_data;
+  reg  [1:0] rx_data_index;
+
+  reg        rx_data_check;
 
   always @(posedge app_clk) begin
-    if (app_tx_overflow) begin
-      $display("FAILED: unexpected tx buffer overflow");
-      $finish;
+    rx_data_check <= 1'b0;
+
+    if (app_rst) begin
+      app_rx_ack_reg   <= 1'b0;
+      rx_counter_valid <= 1'b0;
+      rx_data_index    <= 2'b0;
+      mode_done[MODE_RX_OVERRUN]  <= 1'b0;
+      mode_done[MODE_RX_OVERWAIT] <= 1'b0;
+    end else begin
+      if (rx_data_check) begin
+        rx_counter_valid <= 1'b1;
+        rx_counter       <= rx_data;
+
+        if (rx_data != (rx_counter + 1) && rx_counter_valid) begin
+          $display("FAILED: app rx data mismatch - got %x, expected %x", rx_data, rx_counter + 1);
+          $finish;
+        end
+      end
+
+      case (mode)
+        MODE_TX_OVERFLOW : begin
+          app_rx_ack_reg <= 1'b1;
+        end
+        MODE_TX_OVERWAIT : begin
+          app_rx_ack_reg <= 1'b1;
+        end
+        MODE_RX_OVERRUN  : begin
+          app_rx_ack_reg <= 1'b0;
+          if (app_rx_overrun)
+            mode_done[MODE_RX_OVERRUN] <= 1'b1;
+        end
+        MODE_RX_OVERWAIT : begin
+          app_rx_ack_reg <= 1'b1;
+          if (!app_rx_overrun && app_rx_eof && app_rx_dvld)
+            mode_done[MODE_RX_OVERWAIT] <= 1'b1;
+        end
+        default: begin
+          app_rx_ack_reg <= 1'b1;
+          if (app_rx_dvld && app_rx_ack) begin
+            rx_data_index <= rx_data_index + 1;
+            case (rx_data_index)
+              0: begin
+                rx_data[7:0]  <= app_rx_data;
+              end
+              1: begin
+                rx_data[15:8]  <= app_rx_data;
+              end
+              2: begin
+                rx_data[23:16] <= app_rx_data;
+              end
+              3: begin
+                rx_data[31:24] <= app_rx_data;
+                rx_data_check <= 1'b1;
+              end
+            endcase
+          end
+        end
+      endcase
     end
   end
 
-  assign app_tx_rst = app_rst;
- 
 
+  /******************* MAC *******************/
 
-  /**** MAC TX ****/
+  /* A simple loopback */
+  
 
+  /* TX */
   assign mac_tx_clk = mac_clk;
   reg mac_rst;
   always @(posedge mac_clk) begin
@@ -236,22 +369,7 @@ module TB_gbe_dup();
 
   reg [31:0] foo_index;
 
-  always @(posedge mac_clk) begin
-    if (mac_rst) begin
-      foo_index <= 0;
-    end else begin
-      if (mac_tx_state == 1 || mac_tx_state == 2) begin
-        foo_index <= foo_index + 1;
-        if (mac_tx_dvld)
-          $display("mac_tx: %4d - %x", foo_index, mac_tx_data);
-      end
-      if (mac_tx_state == 3) begin
-        foo_index <= 0;
-      end
-    end
-  end
-
-  /**** MAC RX ****/
+  /* RX */
   assign mac_rx_clk  = mac_clk;
   assign mac_rx_rst  = mac_rst;
   assign mac_rx_data = mac_tx_data;
@@ -262,78 +380,83 @@ module TB_gbe_dup();
   assign mac_rx_goodframe = mac_tx_state == 3 && wait_counter == 10;
   assign mac_rx_badframe  = 1'b0;
 
-  /**** CPU Master ****/
+  /************************ CPU Master ******************************/
 
   reg cpu_rst;
   always @(posedge cpu_clk) begin
     cpu_rst <= rst;
   end
 
-  assign wb_clk_i = cpu_clk;
-  assign wb_rst_i = cpu_rst;
+  reg         cpu_stb;
+  reg         cpu_rnw;
+  reg   [3:0] cpu_sel;
+  reg  [31:0] cpu_adr;
+  reg  [31:0] cpu_dat_wr;
+  wire [31:0] cpu_dat_rd;
+  wire        cpu_ack;
 
-  reg [31:0] cpu_tx_progress;
-  reg [2:0] cpu_state;
-  reg [31:0] cpu_test_count;
-  localparam CPU_WR   = 0;
-  localparam CPU_SEND = 1;
+  reg  [2:0] cpu_state;
+  reg [15:0] cpu_buffer_index;
+  reg        cpu_wait_ack;
+  localparam CPU_IDLE   = 0;
+  localparam CPU_WRWAIT = 1;
+  localparam CPU_WR     = 2;
+  localparam CPU_WRSEND = 3;
+  localparam CPU_RDWAIT = 4;
+  localparam CPU_RD     = 5;
+  localparam CPU_RDSEND = 6;
+  localparam CPU_DONE   = 7;
 
-  reg wait_ack;
+  reg [31:0] cpu_progress;
 
-  reg cpu_stb;
-  reg [31:0] wb_adr_reg;
-  reg        wb_we_reg;
-  reg [31:0] wb_dat_reg;
-
-  assign wb_stb_i = cpu_stb;
-  assign wb_cyc_i = cpu_stb;
-  assign wb_we_i  = wb_we_reg;
-  assign wb_adr_i = wb_adr_reg;
-  assign wb_dat_i = wb_dat_reg;
-  assign wb_sel_i = 4'b1111;
-
-  localparam CPU_TXSIZE = 16'd77;
 
   always @(posedge cpu_clk) begin
-    cpu_stb <= 1'b0;
+    cpu_sel <= 1'b0;
 
     if (cpu_rst) begin
-      cpu_state <= CPU_WR;
-      cpu_tx_progress <= 0;
-      cpu_test_count  <= 0;
-      wait_ack <= 1'b0;
+      cpu_state           <= CPU_IDLE;
+      cpu_wait_ack        <= 1'b0;
+      cpu_progress        <= 32'b0;
+      cpu_buffer_index    <= 16'h0;
+      mode_done[MODE_CPU] <= 1'b0;
     end else begin
-      if (wait_ack) begin
-        if (wb_ack_o || wb_err_o)
-          wait_ack <= 1'b0;
-      end else begin
-        cpu_stb <= 1'b1;
-        case (cpu_state)
-          CPU_WR: begin
-            wait_ack   <= 1'b1;
-            wb_we_reg  <= 1'b1;
-            wb_adr_reg <= 32'h1000 + (cpu_tx_progress[7:0] << 2);
-            wb_dat_reg <= cpu_tx_progress[31:0];
-            cpu_tx_progress <= cpu_tx_progress + 1;
-            if (cpu_tx_progress[7:0] == 63)
-              cpu_state <= CPU_SEND;
+      case (cpu_state)
+        CPU_IDLE : begin
+          if (mode == MODE_CPU) begin
+            cpu_state <= CPU_WR;
           end
-          CPU_SEND: begin
-            wait_ack   <= 1'b1;
-            wb_we_reg  <= 1'b1;
-            wb_adr_reg <= 6*4;
-            wb_dat_reg <= {CPU_TXSIZE, 16'hffff};
-            if (cpu_test_count == 5) begin
-              cpu_state <= 2;
-            end else begin
-              cpu_state <= CPU_WR;
-            end
-            cpu_test_count <= cpu_test_count + 1;
-          end
-        endcase
-      end
+        end
+        CPU_WRWAIT : begin
+        end
+        CPU_WR : begin
+        end
+        CPU_WRSEND : begin
+        end
+        CPU_RDWAIT : begin
+        end
+        CPU_RD : begin
+        end
+        CPU_RDACK : begin
+        end
+        CPU_DONE : begin
+          mode_done[MODE_CPU] <= 1'b0;
+        end
+      endcase
     end
   end
+
+  assign wb_clk_i   = cpu_clk;
+  assign wb_rst_i   = cpu_rst;
+
+  assign wb_stb_i   = cpu_stb;
+  assign wb_cyc_i   = cpu_stb;
+  assign wb_we_i    = !cpu_rnw;
+  assign wb_adr_i   = cpu_adr;
+  assign wb_dat_i   = cpu_dat_wr;
+  assign wb_sel_i   = cpu_sel;
+
+  assign cpu_dat_rd = wb_dat_o;
+  assign cpu_ack    = wb_ack_o;
 
 
 
